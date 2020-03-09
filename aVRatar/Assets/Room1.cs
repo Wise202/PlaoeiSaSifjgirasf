58630dbb57a9a604e5e304aec6949fe4b045f464
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-03-09 22:18:25 +0800

    3

---
+++ aVRatar/Assets/Room1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room1 : MonoBehaviour
+{
+    public bool[] litTorches;
+
+    private void Update()
+    {
+        allTorchesLit();
+    }
+
+    void allTorchesLit() 
+    {
+        if (litTorches[0] && litTorches[1] && litTorches[2] && litTorches[3]) 
+        {
+            
+        }
+    }
+}
