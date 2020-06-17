2bd5290bf4cc907aa852dcad3996cc5c80604c0d
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-06-17 14:34:49 +0800

    11

--- Assets/BetterScripts/Torch.cs
+++ Assets/BetterScripts/Torch.cs
@@ -16,47 +16,58 @@ public class Torch : MonoBehaviour
         if (col.gameObject.name == "Flame")
         {
 
-
-            if (this.gameObject.name == "T1")
-            {
-                boxCol.enabled = false;
-            }
-            if (this.gameObject.name == "T2")
-            {
-                boxCol.enabled = false;
-            }
-            if (this.gameObject.name == "T3")
-            {
-                boxCol.enabled = false;
-            }
-            if (this.gameObject.name == "T4")
-            {
-                boxCol.size = new Vector3(0,0,0);
-            }
-
             switch (cT.torch)
             {
                 case 0:
-                    cT.torch = 1;
-                    
+                    cT.torch += 1;
+                    this.gameObject.layer = 10;
                     cT.onFire[0] = true;
                     break;
                 case 1:
-                    cT.torch = 2; 
+                    cT.torch += 1;
+                    this.gameObject.layer = 10;
                     cT.onFire[1] = true;
                     break;
                 case 2:
-                    cT.torch = 3;
+                    cT.torch += 1;
+                    this.gameObject.layer = 10;
                     cT.onFire[2] = true;
                     break;
                 case 3:
-                    cT.torch = 4;
+                    cT.torch += 1;
+                    this.gameObject.layer = 10;
                     cT.onFire[3] = true;
                     break;
             }
             
         }
 
+        if (col.gameObject.layer == 11)
+        {
+            switch (cT.torch)
+            {
+                case 1:
+                    cT.torch -= 1;
+                    this.gameObject.layer = 0;
+                    cT.onFire[0] = false;
+                    break;
+                case 2:
+                    cT.torch -= 1;
+                    this.gameObject.layer = 0;
+                    cT.onFire[1] = false;
+                    break;
+                case 3:
+                    cT.torch -= 1;
+                    this.gameObject.layer = 0;
+                    cT.onFire[2] = false;
+                    break;
+                case 4:
+                    cT.torch -= 1;
+                    this.gameObject.layer = 0;
+                    cT.onFire[3] = false;
+                    break;
+            }
+        }
 
     }
 
