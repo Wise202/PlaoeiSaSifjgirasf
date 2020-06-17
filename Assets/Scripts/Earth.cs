41e2f78113d641be656c166d01b651f4121f59fa
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-06-17 15:26:19 +0800

    0

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    float deathCount = 0.2f;
+    bool deathCheck;
+
+    void Update()
+    {
+        if (deathCheck)
+        {
+            deathCount -= 1f;
+        }
+
+        if (deathCount <= 0)
+        {
+            
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject)
+        {
+            deathCheck = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject ExitDoor;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Earth")
+        {
+            ExitDoor.SetActive(true);
+            print("11");
+        }
+    }
+}
