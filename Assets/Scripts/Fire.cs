885dcf5ae8e47b5b82a3dfab81643ae7c627af87
SAE Laptop <>
2020-06-18 14:30:57 +0800

    0

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    //this script lights the torches in the scene with a fireball that works like a normal
+    //sphere
+    public GameObject fireAnim;
+
+    public Torches torches;
+
+
+
+    private void Start()
+    {
+        torches = GameObject.FindGameObjectWithTag("Door").GetComponent<Torches>();
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Torch1" && !torches.lit[0])
+        {
+
+            //Instantiate(fireAnim, new Vector3(collision.transform.position.x, collision.transform.position.y + 1.5f, collision.transform.position.z + -0.5f), Quaternion.identity);
+            torches.lit[0] = true;
+        }
+        if (collision.gameObject.name == "Torch2" && !torches.lit[1])
+        {
+            //Instantiate(fireAnim, new Vector3(collision.transform.position.x, collision.transform.position.y + 1.5f, collision.transform.position.z + -0.5f), Quaternion.identity);
+            torches.lit[1] = true;
+        }
+        if (collision.gameObject.name == "Torch3" && !torches.lit[2])
+        {
+            //Instantiate(fireAnim, new Vector3(collision.transform.position.x, collision.transform.position.y + 1.5f, collision.transform.position.z + -0.5f), Quaternion.identity);
+            torches.lit[2] = true;
+        }
+        if (collision.gameObject.name == "Torch4" && !torches.lit[3])
+        {
+            //Instantiate(fireAnim, new Vector3(collision.transform.position.x, collision.transform.position.y + 1.5f, collision.transform.position.z + -0.5f), Quaternion.identity);
+            torches.lit[3] = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/VRTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class VRTest : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean teleportAction;
+    public SteamVR_Action_Boolean grabAction;
+
+
+
+    //put update here
+    private void Update()
+    {
+        if (getTeleportDown())
+        {
+            print("Teleport" + handType);
+        }
+        if (getGrab())
+        {
+            print("Grab " + handType);
+        }
+    }
+
+
+    public bool getTeleportDown()
+    {
+        return teleportAction.GetStateDown(handType);
+    }
+
+    public bool getGrab()
+    {
+        return grabAction.GetState(handType);
+    }
+
+}
