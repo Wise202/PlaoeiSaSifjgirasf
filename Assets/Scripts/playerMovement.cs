ce5eb5c6c9a94a71df9df85cc3b84b1a960e5b7e
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-07-03 12:26:19 +0800

    fps scene with sprint

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        Sprint();
+    }
+
+    void Sprint() 
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 6f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 4f;
+        }
+    }
+}
