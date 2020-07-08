07749b196e70af12d9ba3d75d370bc204971a4d0
Wise202 <51159440+Wise202@users.noreply.github.com>
2020-07-08 16:14:13 +0800

    6

---
+++ Assets/Scripts/CastingScript1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastingScript1 : MonoBehaviour
+{
+    public Transform[] castingPoints;
+    public GameObject Fire;
+
+    float xRotation = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Instantiate(Fire, castingPoints[0].position, Quaternion.identity);
+        }
+
+        float posY = Input.GetAxis("Mouse Y") * 100f * Time.deltaTime;
+
+        xRotation -= posY;
+        xRotation = Mathf.Clamp(xRotation, 0f, 0f);
+
+    }
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -9,16 +9,39 @@ public class playerMovement : MonoBehaviour
 
     public float speed = 4f;
 
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    public float jumpHeight = 3f;
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
-        Sprint();
+        //Sprint();
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+     
     }
 
     void Sprint() 
